41e8ef4e4fdb5f3fce4255e0fc9a4dc6ba33034a
harry18121911 <harrytheripper1812@hotmail.com.ar>
2024-11-26 22:55:13 -0300

    Html Template Adapted.

---
+++ resources/views/home/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class=" footer_section">
+    <div class="container">
+      <p>
+        &copy; <span id="displayyear"></span> all rights reserved by
+        <a href="https://html.design/">web tech knowledge</a>
+      </p>
+    </div>
+</footer>
