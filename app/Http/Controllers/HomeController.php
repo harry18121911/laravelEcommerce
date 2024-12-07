239c9dfde67f2346c53c3faa3e33820603f675b8
harry18121911 <harrytheripper1812@hotmail.com.ar>
2024-12-07 14:25:09 -0300

    Display product date finished

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,15 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view("admin.index");
     }
 
-    public function home(){
-        return view("home.index");
+    public function home()
+    {
+        $product = Product::all();
+
+        return view("home.index", compact("product"));
+    }
+
+    public function login_home()
+    {
+        $product = Product::all();
+
+        return view("home.index", compact("product"));
     }
 }
